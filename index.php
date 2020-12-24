a53218bf0a1ef2bc2527b0134d0fdf32d113ec98
Ivanov-A-V1004 <sawa.ivanov1004@mail.ru>
2020-12-24 16:48:51 +0300

    lab2

--- index.php
+++ index.php
@@ -1,35 +1,44 @@
 <?php
+ini_set("display_errors", 1);
+error_reporting(-1);
 
 class A
 {
+    protected $x;
 
+    public function line($a, $b)
+    {
+        return array(($a === 0 ? Null : $this->x = -$b / $a));
+
+    }
 }
 
 class B extends A
 {
-    public function __construct($a)
+
+    protected function desc($a, $b, $c)
     {
-        $this->a = $a;
+        return $d = $b * $b - 4 * $a * $c;
+
     }
 
-    protected $a;
-}
-
-class C extends B
-{
-    public function __construct($a, $b)
+    public function line($a, $b, $c)
     {
-        $this->b = $b;
-        parent::__construct($a, $b);
+        if ($a === 0) {
+            return parent::eql($b, $c);
+        }
+
+        $d = $this->desc($a, $b, $c);
+
+        if ($d > 0) {
+            $sd = sqrt($d);
+            return $this->x = array((-$b - $sd) / (2 * $a), (-$b + $sd) / (2 * $a));
+        }
+
+        if ($d === 0) {
+            return $this->x = array(-$b / (2 * $a));
+        }
+
+        return null;
     }
-
-    protected $c;
-    protected $b;
 }
-
-$a1 = new A();
-$b2 = new B($a1);
-$b3 = new B($b2);
-$b4 = new B($b3);
-$c5 = new C($a1, $b4);
-var_dump($c5);
