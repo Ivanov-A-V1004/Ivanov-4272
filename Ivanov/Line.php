8765d60fccc5b71e1a5fd64ba8729442dfbd1e83
Ivanov-A-V1004 <sawa.ivanov1004@mail.ru>
2020-12-24 16:50:57 +0300

    lab4

--- Ivanov/Line.php
+++ Ivanov/Line.php
@@ -2,6 +2,8 @@
 
 namespace Ivanov;
 
+use Ivanov\IvanovException;
+
 class Line
 {
     protected $x;
@@ -9,7 +11,9 @@ class Line
     public function line($a, $b)
     {
         if ($a != 0) {
+            MyLog::log("This is a line equation!\n\r");
             return $this->x = array(-$b / $a);
         }
+        throw new IvanovException ('This line equation does not exist!');
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ error_reporting(-1);
 
 use Ivanov\Square;
 use Ivanov\MyLog;
+use Ivanov\IvanovException;
 
 require_once 'core/LogInterface.php';
 require_once 'core/LogAbstract.php';
@@ -11,3 +12,22 @@ require_once 'core/EquationInterface.php';
 require_once 'Ivanov/Line.php';
 require_once 'Ivanov/MyLog.php';
 require_once 'Ivanov/Square.php';
+require_once 'Ivanov/IvanovException.php';
+
+try {
+    echo "Enter 3 numbers\n\r";
+
+    $a = (int)readline();
+    $b = (int)readline();
+    $c = (int)readline();
+
+    MyLog::log("The equation is introduced:" . " $a*x^2 + $b*x + $c = 0");
+    $test = new Square();
+    $r = $test->solve($a, $b, $c);
+    MyLog::log("Roots: " . implode(", ", $r));
+
+} catch (IvanovException $e) {
+    MyLog::log($e->getMessage());
+}
+MyLog::write();
+
